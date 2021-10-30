c311ad16d87f593f8e668b8f7c86054b4be26742
mariosknl <mariosknl@outlook.com>
2021-10-30 21:18:06 +0300

    linechart component created

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -5,6 +5,42 @@ import { Col, Row, Typography } from 'antd'
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for(let i = 0; i <coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory.data.history[i].price)
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price in USD',
+        data: coinPrice,
+        field: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd'
+      }
+    ]
+  }
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
   return (
     <>
       <Row className="chart-header">
@@ -16,7 +52,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
         </Col>
       </Row>
-      {/* <Line data={data} options={options} /> */}
+      <Line data={data} options={options} />
     </>
   )
 }
