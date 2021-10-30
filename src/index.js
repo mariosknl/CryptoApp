8081eb6da70eee0adf031bc0179534f903ae889a
mariosknl <mariosknl@outlook.com>
2021-10-30 07:08:03 +0300

    basic layout implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/antd.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'));
